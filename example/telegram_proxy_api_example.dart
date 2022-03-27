c5384e1f44ab651aca5d72a85165fe3d64fa97d5
amir1430 <amirhossein13791430@gmial.com>
2022-03-27 19:08:19 +0430

    fix bugs and refactor some code

--- example/telegram_proxy_api_example.dart
+++ example/telegram_proxy_api_example.dart
@@ -1,10 +1,10 @@
 import 'package:telegram_proxy_api/telegram_proxy_api.dart';
 
-Future<void> main() async {
+Future<void> malaunchUrlin() async {
   final api = TelegramProxyApiClient();
   final promt = await api.getMtprotos();
   final pros = await api.getsocksts();
 
-  print(promt[0].uri.toString());
-  print(pros[0].uri.toString());
+  print(promt[0].launchUrl.toString());
+  print(pros[0].launchUrl.toString());
 }
